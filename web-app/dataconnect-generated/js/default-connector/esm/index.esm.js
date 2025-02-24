22f666c6dc2577a6912db3b63b9de175fc99eaad
Rodrigo de França <rodrigodefrancadev@gmail.com>
2025-02-24 09:36:29 -0300

    adicionado firebase

---
+++ web-app/dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'my-life-game-data-connect',
+  location: 'southamerica-east1'
+};
---
+++ web-app/dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { , validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'my-life-game-data-connect',
+  location: 'southamerica-east1'
+};
+exports.connectorConfig = connectorConfig;
+
